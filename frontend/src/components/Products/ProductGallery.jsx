cc483fea017d342c9de287ef94939d3d1cbd066e
avliserdna <andre.christian.silva@gmail.com>
2024-06-25 00:22:06 -0500

    started list view

--- frontend/src/components/Products/ProductGallery.jsx
+++ frontend/src/components/Products/ProductGallery.jsx
@@ -18,13 +18,16 @@ const ProductGallery = () => {
 			>
 				Products
 			</div>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
-				{filteredProducts.map((product) => {
+			<div className="container mx-auto p-4">
+			<ul className="bg-white rounded-lg shadow divide-y divide-gray-200">
+			{filteredProducts.map((product) => {
 					return (
             <ProductGalleryCard
             product ={product}/>
 					);
 				})}
+      </ul>
+
 			</div>
 		</div>
   )
